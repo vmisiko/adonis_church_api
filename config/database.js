c4256b3a837cbd9fa8bed6b7fcea8620c499a299
victor misiko <victormisiko.vm@gmail.com>
2021-05-20 02:05:47 +0300

    adde fix in cong data tabes

--- config/database.js
+++ config/database.js
@@ -6,8 +6,8 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
-const URL = require("url-parse");
-const DATABASE_URL = new URL(Env.get("DATABASE_URL"));
+// const URL = require("url-parse");
+// const DATABASE_URL = new URL(Env.get("DATABASE_URL"));
 
 
 module.exports = {
@@ -20,7 +20,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'pg'),
+  connection: Env.get('DB_CONNECTION', 'sqlite'),
 
   /*
   |--------------------------------------------------------------------------
@@ -75,11 +75,11 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.host),
+      host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis')
     }
   }
 }
