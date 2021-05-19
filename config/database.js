55e278ca82d0daadcff9eb40a34bc10ae0872174
victor misiko <victormisiko.vm@gmail.com>
2021-05-19 18:15:27 +0300

    Added database url

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const Env = use('Env')
 const Helpers = use('Helpers')
 
 const URL = require("url-parse");
-const PROD_PG_DB = new URL(Env.get("PG_DATABASE_URL"));
+const DATABASE_URL = new URL(Env.get("DATABASE_URL"));
 
 
 module.exports = {
@@ -54,11 +54,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', DATABASE_URL.host),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      user: Env.get('DB_USER', DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
     }
   },
 
