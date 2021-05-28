cdf29d2f2c84d6bcba2f25ef66b28b606b39a10b
victor misiko <victormisiko.vm@gmail.com>
2021-05-29 00:46:59 +0300

    Added ssl in pg

--- config/database.js
+++ config/database.js
@@ -80,9 +80,9 @@ module.exports = {
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
       database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1)),
-      // ssl: {
-      //   rejectUnauthorized: false
-      // }
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   }
 }
